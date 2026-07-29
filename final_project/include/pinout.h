841cec222da22e39c0574169370fa29ddbb065c7
emmath10 <ehuynh10@protonmail.com>
2026-07-29 13:49:50 -0700

    Added header for pinout definitions

---
+++ final_project/include/pinout.h
@@ -0,0 +1,20 @@
+#ifndef PINOUT_H
+#define PINOUT_H
+
+//I2C
+#define I2C_SCL         22
+#define I2C_SDA         21
+#define TH_ADDR         0x38
+#define OLED_ADDR       0x3C
+
+//GPIO
+#define PIR             19
+#define BUZZER          18
+#define BUTTON_LEFT     5
+#define BUTTON_RIGHT    17
+#define BUTTON_MIDDLE   16
+#define LED_LEFT        4
+#define LED_MIDDLE      2
+#define LED_RIGHT       15
+
+#endif
