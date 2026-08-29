772afb57eae01638c5303fd5131fc9b8fd4b7075
emmath10 <ehuynh10@protonmail.com>
2026-08-28 20:14:20 -0700

    Changed GPIO pinouts

--- final_project/include/pinout.h
+++ final_project/include/pinout.h
@@ -1,20 +1,22 @@
 #ifndef PINOUT_H
 #define PINOUT_H
 
-//I2C
-#define I2C_SCL   22
-#define I2C_SDA   21
-#define TH_ADDR   0x38
+// I2C
+#define I2C_SCL 22
+#define I2C_SDA 21
+#define TH_ADDR 0x38
 #define OLED_ADDR 0x3C
 
-//GPIO
-#define PIR       23
-#define BUZZER    1
-#define BTN_R     19
-#define BTN_M     18
-#define BTN_L     5
-#define LED_R     17
-#define LED_M     16
-#define LED_L     4
+// GPIO Inputs (PIR & Buttons)
+#define PIR 23
+#define BTN_R 19
+#define BTN_M 18
+#define BTN_L 32
+
+// GPIO Outputs (Buzzer & LEDs)
+#define BUZZER 27
+#define LED_R 33
+#define LED_M 25
+#define LED_L 26
 
 #endif
