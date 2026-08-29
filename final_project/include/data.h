77eb3b295f900f10ae355e737b233a23bfa94772
emmath10 <ehuynh10@protonmail.com>
2026-08-28 20:10:40 -0700

    Added Data struct

---
+++ final_project/include/data.h
@@ -0,0 +1,35 @@
+#ifndef DATA_H
+#define DATA_H
+
+#include <Arduino.h>
+
+struct Data {
+  int sleep_movement_ct = 0;
+  bool is_sleeping = false;
+  float curr_hum = 50.0;
+  float curr_temp = 50.0;
+  int water_ct = 0;
+
+  tm wakeupAlarm;
+  tm lastWater;
+  int waterInterval = 0;
+
+  char bleBuffer[64];
+  volatile bool bleNewData = false;
+
+  inline void resetWaterCT() {
+    struct tm timeinfo;
+    if (!getLocalTime(&timeinfo)) {
+      Serial.println("Failed to obtain time");
+      return;
+    }
+    if (timeinfo.tm_hour == 0 && timeinfo.tm_min == 0 &&
+        timeinfo.tm_sec == 0) {
+          water_ct = 0;
+        }
+  }
+};
+
+extern Data curr_data;
+
+#endif
